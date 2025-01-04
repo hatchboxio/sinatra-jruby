4745531875475ba8c4f0bea84d7c507b0a0fb040
Chris Oliver <excid3@gmail.com>
2025-01-04 13:36:44 -0600

    display ruby install name

--- app.rb
+++ app.rb
@@ -2,5 +2,5 @@ require 'bundler/setup'
 require 'sinatra'
 
 get '/' do
-  "Hello world from Sinatra & Ruby #{RUBY_VERSION}!"
+  "Hello world from Sinatra & #{RbConfig::CONFIG["RUBY_INSTALL_NAME"]} #{RUBY_VERSION}!"
 end
