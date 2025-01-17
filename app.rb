04ca2d589927456f66b9eb9d8dfe81c592ec1cad
Chris Oliver <excid3@gmail.com>
2025-01-17 10:04:33 -0600

    Print env in response

--- app.rb
+++ app.rb
@@ -2,6 +2,5 @@ require 'bundler/setup'
 require 'sinatra'
 
 get '/' do
-  p ENV
-  "Hello world from Sinatra & #{RbConfig::CONFIG["RUBY_INSTALL_NAME"]} #{RUBY_VERSION}!"
+  "Hello world from Sinatra & #{RbConfig::CONFIG["RUBY_INSTALL_NAME"]} #{RUBY_VERSION}!<br><br>#{ENV.inspect}"
 end
